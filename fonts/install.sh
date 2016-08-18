73ed58da4cc87f1e1dacdedaffb24c36cd60c7bc
Simon Thörnqvist <simon.thornqvist@gmail.com>
2016-08-18 10:22:02 +0200

    Update fonts install script
    Adds font-awesome

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 echo "Installing fonts ..."
 
-# Create temporary directoyr
+FONT_AWS_GIT="https://github.com/FortAwesome/Font-Awesome.git"
+FONT_PWR_GIT="https://github.com/powerline/fonts.git"
+FONT_PWR_DIR="pwr"
+FONT_AWS_DIR="aws"
+FONT_DIR="$HOME/.local/share/fonts"
+# Create fonts dir if it does not exist
+mkdir -p $FONT_DIR
+# Create temporary folder
 DIR=`mktemp -d`
-# Clone repo
-git clone https://github.com/powerline/fonts.git $DIR
-# run install script
-$DIR/install.sh
+
+cd $DIR
+
+git clone "$FONT_PWR_GIT" "$FONT_PWR_DIR" && ./$FONT_PWR_DIR/install.sh
+git clone "$FONT_AWS_GIT" "$FONT_AWS_DIR" && cp -r ./$FONT_AWS_DIR/fonts/* $FONT_DIR/
+
+# Reset fonts
+echo "Resetting font cache.."
+fc-cache -f $FONT_DIR
 
 # Cleanup temp folder
 function cleanup {
+    echo "Font installation complete!"
     rm -rf "$DIR"
 }
 
